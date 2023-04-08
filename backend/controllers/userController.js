b0e402c5eb37721fa78d147015e947d1874ceb6b
meinjam <injam.cse@gmail.com>
2023-04-08 15:15:22 +0600

    user created done

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,11 +1,25 @@
 const User = require('../models/userModel');
+const bcrypt = require('bcrypt');
 
 const loginUser = async (req, res) => {
   res.status(200).json({ msg: 'login success' });
 };
 
-const signupUser = async (req, res) => {
-  res.status(200).json({ msg: 'signup success' });
+const signupUser = async function (req, res) {
+  const { name, email, password } = req.body;
+
+  const exists = await User.findOne({ email });
+
+  if (exists) {
+    return res.status(500).json({ error: 'Email already in use' });
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  const passwordHash = await bcrypt.hash(password, salt);
+
+  const user = await User.create({ email, password: passwordHash, name });
+
+  res.status(201).json({ email, user });
 };
 
 module.exports = { loginUser, signupUser };
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,7 +1,12 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema(
   {
+    name: {
+      type: String,
+      required: true,
+    },
     email: {
       type: String,
       required: true,
@@ -20,7 +25,24 @@ userSchema.set('toJSON', {
     returnedObject.id = returnedObject._id.toString();
     delete returnedObject._id;
     delete returnedObject.__v;
+    delete returnedObject.password;
+    delete returnedObject.createdAt;
+    delete returnedObject.updatedAt;
   },
 });
+
+userSchema.statics.signup = async (email, password) => {
+  const exists = await this.findOne({ email });
+
+  if (exists) {
+    throw Error('Email already in use');
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  const hash = await bcrypt.hash(password, salt);
+
+  const user = await this.create({ email, password: hash });
+  return user;
+};
 
 module.exports = mongoose.model('User', userSchema);
